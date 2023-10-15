11bc4ea47b20ca8f2b608c226c963f1585ef0dd8
Shrey <sinhashrey2206@gmail.com>
2023-10-15 14:18:27 +0530

    configured redux-toolkit to communicate b/w menu and toolbox

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,53 +1,62 @@
 import classNames from "./index.module.css";
-import { COLORS } from "@/constants";
+import { useSelector } from "react-redux";
+import { COLORS, MENU_ITEMS } from "@/constants";
 
 const Toolbox = () => {
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
+  const showBrushToolOption =
+    activeMenuItem === MENU_ITEMS.PENCIL ||
+    activeMenuItem === MENU_ITEMS.ERASER;
   const updateBrushSize = (e) => {};
 
   return (
     <div className={classNames.toolboxContainer}>
-      <div className={classNames.toolItem}>
-        <h4 className={classNames.toolText}>Stroke Color</h4>
-        <div className={classNames.itemContainer}>
-          <div
-            className={classNames.colorBox}
-            style={{ backgroundColor: COLORS.BLACK }}
-          />
-          <div
-            className={classNames.colorBox}
-            style={{ backgroundColor: COLORS.RED }}
-          />
-          <div
-            className={classNames.colorBox}
-            style={{ backgroundColor: COLORS.GREEN }}
-          />
-          <div
-            className={classNames.colorBox}
-            style={{ backgroundColor: COLORS.BLUE }}
-          />
-          <div
-            className={classNames.colorBox}
-            style={{ backgroundColor: COLORS.ORANGE }}
-          />
-          <div
-            className={classNames.colorBox}
-            style={{ backgroundColor: COLORS.YELLOW }}
-          />
+      {showStrokeToolOption && (
+        <div className={classNames.toolItem}>
+          <h4 className={classNames.toolText}>Stroke Color</h4>
+          <div className={classNames.itemContainer}>
+            <div
+              className={classNames.colorBox}
+              style={{ backgroundColor: COLORS.BLACK }}
+            />
+            <div
+              className={classNames.colorBox}
+              style={{ backgroundColor: COLORS.RED }}
+            />
+            <div
+              className={classNames.colorBox}
+              style={{ backgroundColor: COLORS.GREEN }}
+            />
+            <div
+              className={classNames.colorBox}
+              style={{ backgroundColor: COLORS.BLUE }}
+            />
+            <div
+              className={classNames.colorBox}
+              style={{ backgroundColor: COLORS.ORANGE }}
+            />
+            <div
+              className={classNames.colorBox}
+              style={{ backgroundColor: COLORS.YELLOW }}
+            />
+          </div>
         </div>
-      </div>
-
-      <div className={classNames.toolBrush}>
-        <h4 className={classNames.toolText}>Brush Size</h4>
-        <div className={classNames.itemContainer}>
-          <input
-            type="range"
-            min={1}
-            max={10}
-            step={1}
-            onChange={updateBrushSize}
-          />
-        </div>
-      </div>
+      )}
+       {showBrushToolOption && (
+        <div className={classNames.toolBrush}>
+          <h4 className={classNames.toolText}>Brush Size</h4>
+          <div className={classNames.itemContainer}>
+            <input
+              type="range"
+              min={1}
+              max={10}
+              step={1}
+              onChange={updateBrushSize}
+            />
+          </div>
+    </div>
+    )}
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
+import Board from "@/components/Board";
+import Menu from "@/components/Menu";
+import Toolbox from "@/components/Toolbox";
+
 export default function Home() {
   return (
    <>
-    <Menu />
-    <Toolbox />
+      <Menu />
+      <Toolbox />
+      <Board />
    </>
   )
 }
